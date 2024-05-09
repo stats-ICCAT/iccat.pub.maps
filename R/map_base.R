1f890aebfd8ed7b3b7b4e08ebc4cae750174a33c
Fabio Fiorellato <flowerside@gmail.com>
2024-05-09 13:07:16 +0200

    Updating maps...

--- R/map_base.R
+++ R/map_base.R
@@ -8,13 +8,15 @@
 #' @param fill_color TBD
 #' @param border_color TBD
 #' @param border_line_width TBD
+#' @param force_all_atlantic TBD
 #' @return TBD
 #' @export
 map.atlantic = function(xlim = DEFAULT_XLIM, x_breaks_every = 10,
                         ylim = DEFAULT_YLIM, y_breaks_every = 10,
                         axis_font_size = DEFAULT_AXIS_FONT_SIZE,
                         fill_color = DEFAULT_FILL_COLOR, border_color = DEFAULT_BORDER_COLOR,
-                        border_line_width = DEFAULT_BORDER_LINE_WIDTH) {
+                        border_line_width = DEFAULT_BORDER_LINE_WIDTH,
+                        force_show_all_atlantic = FALSE) {
 
   world = map_data("world")
 
@@ -32,12 +34,15 @@ map.atlantic = function(xlim = DEFAULT_XLIM, x_breaks_every = 10,
              aes(map_id = region),
              fill  = fill_color,
              color = border_color,
-             linewidth = border_line_width) +
+             linewidth = border_line_width)
 
-    # Necessary to add a SF object (in this case a transparent IATTC area) to see the hemisphere showing in the labels
-    geom_sf(data = st_as_sf(ATLANTIC_OCEAN_RAW_GEOMETRY, crs = 4326, wkt = "GEOMETRY_WKT"),
-            fill = "transparent", color = "transparent") +
+  if(force_show_all_atlantic)
+    map =  map +
+      # Necessary to add a SF object (in this case a transparent IATTC area) to see the hemisphere showing in the labels
+      geom_sf(data = st_as_sf(ATLANTIC_OCEAN_RAW_GEOMETRY, crs = 4326, wkt = "GEOMETRY_WKT"),
+              fill = "transparent", color = "transparent")
 
+  map = map +
     c_sf +
 
     scale_x_continuous(breaks = seq(xlim[1] - x_breaks_every, xlim[2] + x_breaks_every, x_breaks_every), guide = guide_axis(n.dodge = 2)) +
